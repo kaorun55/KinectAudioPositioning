8d023a6cbff24bec7b2fdc45b2cbfa311b968eb0
George Birbilis <GitHub1@zoomicon.com>
2015-11-28 00:01:31 +0200

    - Added MainWindow to the Demo project

---
+++ KinectAudioPositioning.Demo.WPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿//Project: KinectAudioPosition (http://KinectAudioPosition.codeplex.com/)
+//Filename: MainWindow.xaml.cs
+//Version: 20151127
+
+using System;
+using System.Windows;
+
+namespace KinectAudioPositioning.Demo.WPF
+{
+
+  /// <summary>
+  /// MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+
+    #region --- Initialization ---
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+    #region --- Events ---
+
+    /// <summary>
+    /// Event handler to care window closing
+    /// KinectMicArray object should be disposed in order to release hardware resources
+    /// </summary>
+    private void Window_Closing(object sender, EventArgs e)
+    {
+      KinectAudioPos.Dispose(); //probably not needed if WPF calls Dispose automatically on controls that implement IDisposable, but to be safe call Dispose anyway (it ignores subsequent calls)
+    }
+
+    #endregion
+
+  }
+}
