432cb57819216f676c8020d59f35726a0579c99a
George Birbilis <GitHub1@zoomicon.com>
2015-12-01 19:41:44 +0200

    - fixed issue with KinectAudioPositioning.dll not loading - updated NuGet packages

--- KinectAudioPositioning.Demo.WPF/Properties/Resources.Designer.cs
+++ KinectAudioPositioning.Demo.WPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KinectAudioPositioning.Demo.WPF.Demo.WPF.Properties {
+namespace KinectAudioPositioning.Demo.WPF.Properties {
     using System;
     
     
